468e38ac1a888e61f32e489c32a65fcaf45a5b4e
laurafoes <foeslaura@gmail.com>
2023-01-08 10:23:05 -0300

    styles: tailwind theme configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Circular Std', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'cardColor': '#2A2141',
+        'primary': '#E1E1E6',
+        'secondary': '#E1E1E6',
+        'disabled': '#cccccc',
+        'active': '#B199CC',
+        'background': '#0F0D13',
+        'spotify': '#1DB954',
+      },
+      fontSize: {
+        'bodySize': '18px',
+        'titleSize': ' 24px',
+        'timeSize': '14px',
+      },
+    },
   },
   plugins: [],
-};
+}
