11e285522f2dbcdf1b60f9d2e70ba7840a83f957
laurafoes <foeslaura@gmail.com>
2023-01-09 18:40:58 -0300

    styles: theme and minor project configs updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,13 +14,17 @@ module.exports = {
       colors: {
         'cardColor': '#2A2141',
         'primary': '#E1E1E6',
-        'secondary': '#E1E1E6',
+        'secondary': '#D9D9D9',
         'disabled': '#cccccc',
         'active': '#B199CC',
         'background': '#0F0D13',
+        'playerBg': 'rgba(217, 217, 217, 0.3)',
+        'hoverPlayerBg': 'rgba(42, 33, 65, 0.7)',
         'spotify': '#1DB954',
       },
       fontSize: {
+        'smallSize': '14px',
+        'mediumSize': '16px',
         'bodySize': '18px',
         'titleSize': ' 24px',
         'timeSize': '14px',
